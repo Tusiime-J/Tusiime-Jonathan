af9b32050990557d76ac596a41ad30cd7c0c98af
Tusiime J <tusiimejonah@yahoo.com>
2025-06-24 12:28:46 +0300

    added

---
+++ day1_ml/learn_machinelearning.py
@@ -0,0 +1,39 @@
+#machine learning
+#intro
+
+#applications of machine learning
+#1. image analysis
+#2. speech recognition
+#3. disease prediction
+#4. stock market analysis
+#5. recommendation systems
+#6. fraud detection
+#7. stock market prediction
+
+#types of machine learning
+#1. supervised learning -labelled datam, structured data, image classification 
+'''
+Common algorithms:
+- Linear Regression
+- Logistic Regression
+- Decision Trees
+- Support Vector Machines (SVM)
+- K-Nearest Neighbors (KNN)
+- Random Forests
+
+- Deep Learning (CNN) -Image classification building .h5
+'''
+#2. unsupervised learning -unlabelled data, structured data, clustering, discovery of hidden patterns
+'''
+- K-Means Clustering
+- Hierarchical Clustering
+- Principal Component Analysis (PCA)
+'''
+
+#Module selection and validation
+
+
+#3. semi-supervised learning
+#4. reinforcement learning
+
+#machine learning in action 
